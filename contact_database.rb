801767ef45175fc2a10595ad8c9602f753d4a692
Andy Theriault <artheriault@gmail.com>
2014-11-04 04:20:46 +0000

    implemented list and new functions

--- contact_database.rb
+++ contact_database.rb
@@ -1,2 +1,31 @@
 ## TODO: Implement CSV reading/writing
 require 'csv'
+
+class ContactDatabase
+  @contact_db = "contacts.csv"
+
+  def self.list_contacts
+    # File.open(@contact_db,"r") do |file|
+    #   puts file.readline.inspect
+    # end
+    # CSV.foreach(@contact_db) do |row|
+    #   puts "Name: #{row[0]} Email: #{row[1]}"
+    # end
+
+    contacts_array = CSV.read(@contact_db)
+    contacts_array.each_with_index do |contact, row|
+      puts "Name: #{contact[0]} Email: #{contact[1]} ID #{row + 1}"
+    end
+  end
+
+  def self.insert_new_contact(contact)
+    CSV.open(@contact_db, "a") do |csv|
+      csv.add_row(contact)
+    end
+
+
+    contacts_array = CSV.read(@contact_db)
+    puts "an index #{contacts_array.index(contacts_array[-1]) + 1}"
+  end
+
+end
--- contact_list.rb
+++ contact_list.rb
@@ -1,35 +1,86 @@
 require_relative 'contact'
-require_relative 'contact_database'
+# require_relative 'contact_database'
 
-def start
+class Application
 
-  input = ARGV.first
+  def initialize
+    start
+  end
 
-  case input.downcase
-  when "-h", "help", nil
-    puts "Here is a list of available commands:"
-    puts "\t new - Create a new contact"
-    puts "\t list - List all contacts"
-    puts "\t show - Show a contact"
-    puts "\t find - Find a contact"
-  when "new"
-    # run new command
-  when "list"
-    puts "list test"
-    #run list command
-  when "show"
-    #run show command
-  when "find" # use regex Here
-    #run find command
-  else
-    puts "Unknown command"
-    puts "Here is a list of available commands:"
-    puts "\t new - Create a new contact"
-    puts "\t list - List all contacts"
-    puts "\t show - Show a contact"
-    puts "\t find - Find a contact"
+  def   start
+    input = ARGV
+    menu(input)
+  end
+
+  def menu(input = "-h")
+
+    case input.first
+    when "-h", "help", nil
+      puts "Here is a list of available commands:"
+      puts "\t new - Create a new contact"
+      puts "\t list - List all contacts"
+      puts "\t show - Show a contact"
+      puts "\t find - Find a contact"
+      # print '> '
+      # menu(STDIN.gets.chomp)
+
+    when "new"
+      # run new command
+      new_contact
+    when "list"
+      #run list command
+      list_contacts
+    when "show"
+      #run show command
+
+    when "find" # use regex Here
+      #run find command
+    when "exit"
+      return
+    else
+      puts "Unknown command"
+      puts "Here is a list of available commands:"
+      puts "\t new - Create a new contact"
+      puts "\t list - List all contacts"
+      puts "\t show - Show a contact"
+      puts "\t find - Find a contact"
+      # print '> '
+      # menu(STDIN.gets.chomp)
+    end
+  end
+
+  def new_contact
+    puts "Contact Entry Mode"
+    puts "******************************************"
+    puts "Enter full name for new contact."
+    print '> '
+    name = STDIN.gets.chomp()
+    puts "Enter email address for new contact."
+    print '> '
+    email = STDIN.gets.chomp()
+    puts "Name: #{name}"
+    puts "Email: #{email}"
+    puts "Please confirm contact information is correct."
+    print '> '
+    if STDIN.gets.chomp() == "yes"
+      Contact.create(name,email)
+    else
+      new_contact
+    end
+
+    # print '> '
+    # menu(STDIN.gets.chomp())
+    # menu
+  end
+
+  def list_contacts
+    puts "Contact List"
+    puts "******************************************"
+    Contact.list
+    # print '> '
+    # menu(STDIN.gets.chomp())
   end
 
 end
 
-start
+app = Application.new
